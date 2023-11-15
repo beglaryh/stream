ffbd0b03b7952be86ca5db675005a5296e6124fd
beglaryh <harry.beglaryan@adp.com>
2023-11-15 00:13:25 -0800

    Initial Commit

---
+++ stream.go
@@ -0,0 +1,87 @@
+package stream
+
+type Stream[T any] struct {
+	ts []T
+}
+
+func Of[T any](ts []T) Stream[T] {
+	return Stream[T]{ts: ts}
+}
+
+func (stream Stream[T]) Filter(filter func(t T) bool) Stream[T] {
+	ns := Stream[T]{}
+	for _, t := range stream.ts {
+		if filter(t) {
+			ns.ts = append(ns.ts, t)
+		}
+	}
+	return ns
+}
+
+func Map[F, T any](fs []F, mapper func(f F) T) Stream[T] {
+	ns := Stream[T]{}
+	for _, e := range fs {
+		nv := mapper(e)
+		ns.ts = append(ns.ts, nv)
+	}
+	return ns
+}
+
+func (stream Stream[T]) Sort(sortFunction func(a, b T) bool) Stream[T] {
+	ns := mergeSort[T](stream.ts, sortFunction)
+	return Stream[T]{ts: ns}
+}
+
+func (stream Stream[T]) AnyMatch(anyFunction func(t T) bool) bool {
+	for _, e := range stream.ts {
+		if anyFunction(e) {
+			return true
+		}
+	}
+	return false
+}
+
+func (stream Stream[T]) NoneMatch(anyFunction func(t T) bool) bool {
+	for _, e := range stream.ts {
+		if anyFunction(e) {
+			return false
+		}
+	}
+	return true
+}
+
+func mergeSort[T any](es []T, compare func(a, b T) bool) []T {
+	if len(es) < 2 {
+		return es
+	}
+	mid := len(es) / 2
+	left := mergeSort(es[:mid], compare)
+	right := mergeSort(es[mid:], compare)
+
+	var sorted []T
+	i, j := 0, 0
+	for len(sorted) != len(es) {
+		if i == len(left) {
+			sorted = append(sorted, right[j])
+			j += 1
+		} else if j == len(right) {
+			sorted = append(sorted, left[i])
+			i += 1
+		} else {
+			lv := left[i]
+			rv := right[j]
+			if compare(lv, rv) {
+				sorted = append(sorted, lv)
+				i += 1
+			} else {
+				sorted = append(sorted, rv)
+				j += 1
+			}
+		}
+	}
+	return sorted
+}
+
+func (stream Stream[T]) Slice() []T {
+	return stream.ts
+}
---
+++ stream_test.go
@@ -0,0 +1,80 @@
+package stream
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type person struct {
+	name string
+	age  int
+}
+
+func TestStream_Filter(t *testing.T) {
+	numbers := []int{1, 2, 3}
+	filteredNumbers := Of[int](numbers).
+		Filter(func(n int) bool { return n > 1 }).
+		Slice()
+	assert.Equal(t, 2, filteredNumbers[0])
+	assert.Equal(t, 3, filteredNumbers[1])
+	assert.Equal(t, 2, len(filteredNumbers))
+}
+
+func TestStructFilter(t *testing.T) {
+	persons := []person{{"Bob", 50}, {"Bob", 100}, {"Alice", 30}}
+	filteredNumbers := Of[person](persons).Filter(func(p person) bool { return p.name == "Bob" }).Slice()
+	assert.Equal(t, person{"Bob", 50}, filteredNumbers[0])
+	assert.Equal(t, person{"Bob", 100}, filteredNumbers[1])
+	assert.Equal(t, 2, len(filteredNumbers))
+
+	filteredNumbers = Of[person](persons).Filter(func(p person) bool {
+		return p.name == "Bob" && p.age == 100
+	}).Slice()
+	assert.Equal(t, person{"Bob", 100}, filteredNumbers[0])
+	assert.Equal(t, 1, len(filteredNumbers))
+}
+
+func TestStream_Map(t *testing.T) {
+	persons := []person{{"Bob", 50}, {"Bob", 100}, {"Alice", 30}}
+	names := Map[person, string](persons, func(p person) string {
+		return p.name
+	}).Filter(func(name string) bool { return name != "Bob" }).
+		Slice()
+
+	assert.Equal(t, "Alice", names[0])
+	assert.Equal(t, 1, len(names))
+}
+
+func TestStream_Sort(t *testing.T) {
+	numbers := []int{5, 4, 3, 2, 1}
+	sorted := Of(numbers).Sort(func(a, b int) bool { return a < b })
+	assert.Equal(t, []int{1, 2, 3, 4, 5}, sorted.Slice())
+
+	persons := []person{{"Bob", 100}, {"Bob", 50}, {"Alice", 30}}
+	sortedPersons := Of[person](persons).Sort(func(a, b person) bool {
+		if a.name == b.name {
+			return a.age < b.age
+		}
+		return a.name < b.name
+	})
+
+	assert.Equal(t, []person{{"Alice", 30}, {"Bob", 50}, {"Bob", 100}}, sortedPersons.Slice())
+}
+
+func TestStream_AnyMatch(t *testing.T) {
+	numbers := []int{5, 4, 3, 2, 1}
+	match := Of(numbers).AnyMatch(func(a int) bool { return a == 1 })
+	assert.Equal(t, true, match)
+
+	match = Of(numbers).AnyMatch(func(a int) bool { return a == 6 })
+	assert.Equal(t, false, match)
+}
+
+func TestStream_NoneMatch(t *testing.T) {
+	numbers := []int{5, 4, 3, 2, 1}
+	match := Of(numbers).NoneMatch(func(a int) bool { return a == 1 })
+	assert.Equal(t, false, match)
+
+	match = Of(numbers).NoneMatch(func(a int) bool { return a == 6 })
+	assert.Equal(t, true, match)
+}
